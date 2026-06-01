33fb29497573dcbda05e6c9f33e7d1273d923edc
its-r universe <seengarali818@gmail.com>
2026-06-01 14:40:25 +0500

    fix: add missing lib/supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const service = process.env.SUPABASE_SERVICE_ROLE_KEY!
-export const supabaseAdmin = createClient(url, service)
-export const supabase = createClient(url, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+
+const supabaseUrl = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY || ''
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
